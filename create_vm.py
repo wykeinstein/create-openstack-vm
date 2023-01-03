e4fd3a60904f5940f3c4fe1d3bd22608f71bd468
wykeinstein <wykeinstein@outlook.com>
2023-01-03 22:32:35 +0800

    完善v1.1

--- create_vm.py
+++ create_vm.py
@@ -16,120 +16,108 @@ def set_sys_env(env_file):
             if ret:
                 os.environ[ret.group(1)] = ret.group(2)
 
-def get_flavor(flavors=list(), vcpus=int(), ram=int()):
+def construct_server_bdm(row):
+    if 'image' in row.keys() and 'root_disk_size' in row.keys():
+        bdm = {
+            "boot_index": "0",
+            "destination_type": "volume",
+            "source_type": "image",
+            #"uuid": (get_image(row["image"])).id,
+            "destination_type": "volume",
+            "volume_type": row["vol_type"],
+            "volume_size": row["root_disk_size"]
+        }
+    elif 'data_disk_size' in row.keys():
+        bdm = {
+            "destination_type": "volume",
+            "volume_type": row["vol_type"].strip(),
+            "volume_size": row["data_disk_size"],
+            "source_type": "blank",
+        }
+    else:
+        pass
+    return bdm
+
+def get_network(conn, name_or_id, subnet, subnets=list()):
+    for sn in subnets:
+        if subnet == getattr(sn, "cidr"):
+            neutron_net = conn.network.find_network(getattr(sn, "network_id"))
+            if getattr(neutron_net, "provider_network_type") == None:
+                raise ValueError("The user does not have administrator privileges, please give the user administrator privileges")
+            if getattr(neutron_net, "provider_segmentation_id") == None:
+                raise ValueError("The user does not have administrator privileges, please give the user administrator privileges")
+            if name_or_id == getattr(neutron_net, "name") or name_or_id == getattr(neutron_net, "id"):
+                return neutron_net
+            else:
+                raise ValueError('Network %s with subnet %s not found' % (name_or_id, subnet))
+
+def get_flavor(conn, flavors=list(), vcpus=int(), ram=int()):
+    flavor_name = str(vcpus) + "C." + str(ram) + "G"
+    f = None
     for flavor in flavors:
         if vcpus == getattr(flavor, "vcpus") and ram*1024 == getattr(flavor, "ram") and getattr(flavor, "extra_specs") == dict():
-            return flavor
-
-def get_network(subnets=list(), network=str(),net_type=str(), segmentation_id=int()):
-    for subnet in subnets:
-        if network == getattr(subnet, "cidr"):
-           neutron_net = conn.network.find_network(getattr(subnet, "network_id"))
-           if net_type == getattr(neutron_net, "provider_network_type") and segmentation_id == getattr(neutron_net, "provider_segmentation_id"):
-                return neutron_net
-
-def get_image(image_name):
-    #return conn.compute.find_image(image_name)
-    return conn.image.find_image(image_name)
-
-
-
-#def create_server(name=str(), image=str(), flavor=str(), nics=list(), bdms=list(), networks=list()):
-#    image = conn.compute.find_image(image)
-#    flavor = conn.compute.find_flavor(flavor)
-#    server = conn.compute.create_server(name=name, image_id=image.id, flavor_id=flavor.id, nics=nics, block_device_mapping=bdms, networks=networks)
-#    server = conn.compute.wait_for_server(server) 
-#
-
-
-
-
-#network = get_network(subnets=subnets, network="192.168.1.0/24", net_type="vxlan", segmentation_id=1)
-#flavor = get_flavor(flavors=flavors, vcpus=1, ram=1024)
-#bdms = [{"boot_index": "0", "destination_type": "volume", "uuid": image.id, "source_type": "image", "volume_size": "10"},{"destination_type": "volume", "source_type": "blank", "volume_size": "5"},{"destination_type": "volume", "source_type": "blank", "volume_size": "5"}]
-#networks = [{"uuid": network.id, "fixed_ip": "192.168.1.200"}, {"uuid": network.id, "fixed_ip": "192.168.1.201"}]
-
-#server = conn.compute.create_server(name="openstack_sdk_test", image_id=image.id, flavor_id=flavor.id, block_device_mapping=bdms, networks=networks, config_driver=True)
-#server = conn.compute.wait_for_server(server)
-server = dict()
-server["bdms"] = list()
-
-def construct_nova_server_dict(row, server=None):
-    if server == None:
-        server = dict()
-    server["name"] = row["name"].strip()
-    server["flavor"] = get_flavor(flavors, row["vcpus"], row["ram"])
-    server["availability_zone"] = row["zone"].strip()
-    server["image"] = get_image(row["image"])
-    server["nics"] = list()
-    server["vol_type"] = row["vol_type"].strip()
-    netids = list()
-    # row["segmentation_id"]中的数值类型为整型，需要先转换为str类型
-    for net_addr, segmentation_id in zip(row["networks"].split(), str(row["segmentation_id"]).split()):
-        net = get_network(subnets=subnets, network=net_addr, net_type=row["net_type"], segmentation_id=int(segmentation_id))
-        netids.append(net.id)
-    netids_ips = list(zip(netids, row["ips"].split()))
-    netids_ips_dict = dict()
-    for i in range(len(netids_ips)):
-        netids_ips_dict["uuid"] = netids_ips[i][0]
-        netids_ips_dict["fixed_ip"] = netids_ips[i][1]
-        server["nics"].append(deepcopy(netids_ips_dict))
-    server["bdms"] = list()
-    vol_num = int()
-    if isinstance(row["vol_size"], int):
-        vol_num = 1
-    if isinstance(row["vol_size"], str):
-        vol_num = len(row["vol_size"].split())
-    for i in range(vol_num):
-        bdm = dict()
-        if i == 0:
-            bdm["boot_index"] = "0"
-            bdm["destination_type"] = "volume"
-            bdm["source_type"] = "image"
-            bdm["uuid"] = (get_image(row["image"])).id
-            bdm["destination_type"] = "volume"
-            if isinstance(row["vol_size"], int):
-                bdm["volume_size"] = row["vol_size"]
-            if isinstance(row["vol_size"], str):
-                bdm["volume_size"] = row["vol_size"].split()[i]
-        else:
-            bdm["destination_type"] = "volume"
-            bdm['volume_type'] = row['vol_type'].strip()
-            bdm["volume_size"] = row["vol_size"].split()[i]
-            bdm["source_type"] = "blank"
-        server["bdms"].append(deepcopy(bdm))
-    return server
+            f = flavor
+            return f
+    if f is None:
+        return conn.create_flavor(flavor_name, row['ram']*1024, row['vcpus'], 0)
+def create_server(server_dict, conn):
+    msg = ("creating %s" % server_dict['name'])
+    pbar = tqdm(total=100, desc=msg)
+    server_obj = conn.compute.create_server(availability_zone=server_dict["availability_zone"],
+                                            name=server_dict["name"], image_id=(server_dict["image"]).id,
+                                            flavor_id=(server_dict["flavor"]).id,
+                                            block_device_mapping_v2=server_dict["bdms"], networks=server_dict["nics"],
+                                            config_drive=True)
+    pbar.update(50)
+    conn.compute.wait_for_server(server_obj, wait=3600)
+    pbar.update(50)
 
 if __name__ == "__main__":
+    # 将当前脚本执行目录设置为工作目录，并设置默认的配置文件
+    cur_path = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(cur_path)
+    default_conf_path = [os.path.join(cur_path, "config.ini")]
+
     CONF = cfg.CONF
     xls_path = cfg.StrOpt('xls', default=None, help='path to xls file')
     auth_file = cfg.StrOpt('auth', default=None, help='path to admin-openrc.sh auth file')
+    green_pool_size = cfg.StrOpt('pool_size', default=None, help='size of greenpool')
     CONF.register_opt(xls_path)
     CONF.register_opt(auth_file)
-    CONF(default_config_files='config.ini')
-
+    CONF.register_opt(green_pool_size)
+    CONF(default_config_files=default_conf_path)
     set_sys_env(CONF["auth"])
-
     conn = openstack.connect(region_name='RegionOne')
     flavors = [flavor for flavor in conn.compute.flavors()]
     subnets = [subnet for subnet in conn.network.subnets()]
-    images = [image for image in conn.compute.images()]
-    #image = conn.compute.find_image("cirros-0.3.4-x86_64-disk.raw")
+    df = pd.read_excel(io=os.path.abspath(CONF["xls"]), header=2, sheet_name=[0, 1, 2])
 
-    df = pd.read_excel(io=os.path.abspath(CONF["xls"]), header=2)
+    eventlet.monkey_patch(thread=False)
+    pool = eventlet.GreenPool(CONF['pool_size'])
 
-    for index, row in df.iterrows():
-        server_dict = construct_nova_server_dict(row, server=dict())
-        conn.image.update_image_properties(image=server_dict["image"], cinder_img_volume_type=server_dict["vol_type"])
-        while True:
-            if server_dict["image"].properties.get("cinder_img_volume_type") == server_dict["vol_type"]:
-                break
-        print("creating %s" % server_dict['name'])
-        server_obj = conn.compute.create_server(availability_zone=server_dict["availability_zone"], name=server_dict["name"], image_id=(server_dict["image"]).id, flavor_id=(server_dict["flavor"]).id, block_device_mapping_v2=server_dict["bdms"], networks=server_dict["nics"], config_drive=True)
-        server_obj = conn.compute.wait_for_server(server_obj)
-        print("%s created successfully \n" % server_dict["name"])
-        #server = conn.compute.create_server(availability_zone=server["availability_zone"], name=server["name"], image_id=(server["image"]).id, flavor_id=(server["flavor"]).id, networks=server["nics"], config_drive=True, boot_from_volume=True, volume_size='200')
+    vms = df[0]['name'].unique()
+    vm_dict_list = list()
+    for vm in vms:
+        vm_dict = {}
+        vm_dict['name'] = vm
+        vm_dict['bdms'] = list()
+        vm_dict['nics'] = list()
+        for index, row in df[0].iterrows():
+            if row['name'] == vm:
+                neutron_net = get_network(conn, row['network'], row['subnet'], subnets)
+                print('neutron net is %s' % neutron_net)
+                vm_dict['nics'].append(deepcopy(construct_server_nic(neutron_net, row)))
+                vm_dict['bdms'].append(deepcopy(construct_server_bdm(row)))
+                vm_dict['flavor'] = get_flavor(conn, flavors=flavors, vcpus=row['vcpus'], ram=row['ram'])
+        for index, row in df[1].iterrows():
+            if row['name'] == vm:
+                vm_dict['bdms'].append(deepcopy(construct_server_bdm(row)))
+        for index, row in df[2].iterrows():
+            if row['name'] == vm:
+                neutron_net = get_network(conn, row['network'], row['subnet'], subnets)
+                vm_dict['nics'].append(deepcopy(construct_server_nic(neutron_net, row)))
 
-    time.sleep(10)
-    (out, err) = processutils.execute("nova", "list")
-    print(out)
+        vm_dict_list.append(vm_dict)
+    for vm in vm_dict_list:
+        pool.spawn(create_server, vm, conn)
+
